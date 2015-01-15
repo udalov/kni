3e1a7be37d36f1cc20a7b28ca3943e83254b2407
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-01-15 16:07:10 +0300

    Download jnr-ffi and dependencies statically

--- tests/src/CFlavorsTests.kt
+++ tests/src/CFlavorsTests.kt
@@ -4,7 +4,6 @@ package org.jetbrains.kni.tests
 import org.jetbrains.kni.indexer.NativeIndexingOptions
 import java.io.File
 import kotlin.properties.Delegates
-import org.jboss.shrinkwrap.resolver.api.maven.Maven
 
 public abstract class ObjCTest : AbstractIntegrationTest(NativeIndexingOptions(objC = true)) {
 
@@ -29,8 +28,17 @@ public abstract class CPlusPlusTest : AbstractIntegrationTest(NativeIndexingOpti
     override protected fun src2implementation(source: String): String = source.replace(".kt", ".cpp")
 
     override protected val kotlinLibs: List<File> by Delegates.lazy {
-        val jnrref = "com.github.jnr:jnr-ffi:2.0.1"
-        Maven.resolver().resolve(jnrref).withTransitivity().asFile().toArrayList()
+        val targets = listOf(
+                File("lib/jnr-ffi-2.0.1.jar"),
+                File("lib/jffi-1.2.7.jar"),
+                File("lib/jffi-1.2.7-native.jar"),
+                File("lib/asm-all-5.0.3.jar"),
+                File("lib/jnr-x86asm-1.0.2.jar")
+        )
+        for (target in targets) {
+            assert(target.exists()) { "$target is not found. Execute 'ant -f update_dependencies.xml' and 'ant dist' before running tests" }
+        }
+        targets
     }
 
     override protected fun compileNative(source: File, target: File) {
