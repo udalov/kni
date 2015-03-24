22e21c7107f38d13924c2b6d43c1cb95d785b251
ligee <ilya.chernikov@jetbrains.com>
2015-03-24 15:58:02 +0100

    supporting binary output in kniidx

--- indexer/native/kniidx.cc
+++ indexer/native/kniidx.cc
@@ -1,7 +1,7 @@
 
 #include <iostream>
-#include <boost/range/adaptors.hpp>
-#include <boost/range/algorithm.hpp>
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 #include "Indexer.hh"
 #include "NativeIndex.pb.h"
 
@@ -12,10 +12,22 @@ int main(int argc, char *argv[]) {
     else {
         TranslationUnit tu;
         auto vargs = std::vector<std::string>();
-        for (int i = 1; i < argc; ++i) vargs.push_back(argv[i]);
+        bool bin_format = false;
+        const std::string bin_fmt_key("---b");
+        for (int i = 1; i < argc; ++i)
+            if (bin_fmt_key == argv[i]) bin_format = true;
+            else vargs.push_back(argv[i]);
         auto message = doIndexToString(vargs);
         tu.ParseFromString(message);
 
-        std::cout << tu.DebugString() << std::endl;
+        if (bin_format)
+            tu.SerializeToOstream(&std::cout);
+        else
+        {
+            google::protobuf::io::OstreamOutputStream stream(&std::cout);
+            google::protobuf::TextFormat::Print(tu, &stream);
+            // seems the same, but we need explicit TextFormat to parse on the other side
+//            std::cout << tu.DebugString() << std::endl;
+        }
     }
 }
