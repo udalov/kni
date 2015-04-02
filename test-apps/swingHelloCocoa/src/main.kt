06a9ecfeea9bc1911399b2ec76f0b1a3a839ef05
ligee <ilya.chernikov@jetbrains.com>
2015-04-03 00:05:48 +0200

    Experiments with swingHelloCocoa app

--- test-apps/swingHelloCocoa/src/main.kt
+++ test-apps/swingHelloCocoa/src/main.kt
@@ -2,10 +2,39 @@
 package swingHelloCocoa
 
 import javax.swing.JFrame
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
+import objc.*
+
+class JTextAreaOutputStream(val textArea: JTextArea): java.io.OutputStream() {
+    override fun write(b: Int) {
+        textArea.append(b.toChar().toString())
+        textArea.setCaretPosition(textArea.getDocument().getLength())
+    }
+}
 
 fun main(args : Array<String>) {
     val w = JFrame("Title")
     w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
     w.setSize(600, 400)
     w.setVisible(true)
+    val textArea = JTextArea(5, 10)
+    val scrollPane = JScrollPane(textArea)
+    val taStream = java.io.PrintStream(JTextAreaOutputStream(textArea))
+    //System.setOut(taStream)
+    //System.setErr(taStream)
+    println("swing app started")
+    val cocoaApp = NSApplication.sharedApplication() as NSApplication
+    println("cocoa app taken")
+    println("cocoa app is running == ${cocoaApp.isRunning()}")
+    val iconImg = cocoaApp.applicationIconImage().copy() as NSImage
+    iconImg.lockFocus()
+    val rot = NSAffineTransform.transform()
+    rot.rotateByDegrees(90.0)
+
+    iconImg.unlockFocus()
+    cocoaApp.setApplicationIconImage(iconImg)
+    println("icon set")
 }
